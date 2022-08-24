7a0df2dc7f320cc2dd7ca07c13d6016fb572569d
syuulee <yuyu00lee@gmail.com>
2022-08-24 16:31:22 +0900

    2022.08.24.

---
+++ week10/js/main.js
@@ -0,0 +1,16 @@
+// document.querySelector('.intro_total').addEventListener('on')
+//     .classList.remove('on');
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
