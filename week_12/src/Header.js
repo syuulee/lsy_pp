e8724498fe8197cc87e9e925cb725ed6682693fe
syuulee <yuyu00lee@gmail.com>
2022-09-07 17:40:29 +0900

    2022.09.07.

---
+++ week_12/src/Header.js
@@ -0,0 +1,26 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>
+                    <a href=''>
+                        <img src="./img/logo.svg" alt="" />
+                    </a>
+                </h1>
+                <nav className="gnb">
+                    <ul>
+                        <li><a href="">홈</a></li>
+                        <li><a href="">체험/구매</a></li>
+                        <li><a href="">갤러리</a></li>
+                        <li><a href="">브랜드스토리</a></li>
+                    </ul>
+                </nav>
+                <div className="icon">
+                    
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
